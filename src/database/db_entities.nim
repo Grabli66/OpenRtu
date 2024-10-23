89868f569b409c29ed234a7ec078359a8e2921b8
Grabli66 <grabli66@gmail.com>
2024-10-23 21:04:23 +0300

    Добавлены сущности базы данных

---
+++ src/database/db_entities.nim
@@ -0,0 +1,29 @@
+# Сущности базы данных
+
+type 
+    DbEntity* = ref object of RootObj
+        # Идентификатор сущности
+        id*:int
+
+    # Модель прибора учета
+    DbDevice* = ref object of DbEntity   
+        # Идентификатор модели
+        model_type_id*:int
+        # Специфические настройки модели в формате JSON
+        settings*:string
+
+    # Тип прибора учета
+    DbDeviceType* = ref object of DbEntity
+        # Идентификатор модели
+        model_type_id*:int
+        # Название модели
+        name*:string
+
+    # Маршрут до устройства
+    DbRoute* = ref object of DbEntity
+        # Идентификатор устройства к которому относится маршрут
+        device_id*:int
+        # Тип маршрута устройства
+        route_type_id*:int
+        # Настройки маршрута
+        settings*:string
